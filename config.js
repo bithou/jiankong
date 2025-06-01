cc6386765e88ff0b570fdb239fb77bdd648daabd
bithou <132659375+bithou@users.noreply.github.com>
2025-06-01 13:41:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'u2965053-3074c7613b9ee6c723293149',
+    'ur2965053-57f10954c7888621b6ad1933',
     //'m797638073-924f5efed86446cce1c95ae1', // 五毛钱的博客 API key
     //'m797713213-1c83620d56db599960ad0244', // 五毛钱的公益订阅 API key
     //'m797713331-835664f191b589688f122e00', // 五毛钱的公益订阅器 API key
